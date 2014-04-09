db6656dffbc532ceb5655c4d6090bd60f462ad22
Prelang Builder <builder@prelang.com>
2014-04-09 11:53:33 +0000

    Injected Google Analytics tracker ID configuration into development configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,9 @@ Kitchen2::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #->Prelang
+  GA.tracker = ""
+
 end
+
